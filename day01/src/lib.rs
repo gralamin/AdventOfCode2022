0d0deef562764d0826ece2c466ab79cdf5a783ee
Glen Nelson <glen.nelson.1@gmail.com>
2022-11-30 22:41:51 -0700

    Better algortihm for day01.
    This makes puzzle_a a bit slower, but overall is much better.
    Simply precompute the sums [O(n)]
    Then sort it. [O(n log n)]
    Then just pop the last x elements, done.
    
    This makes puzzle_b O(n log n), and just as fast as puzzle_a.

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -10,34 +10,13 @@ pub use filelib::split_lines_by_blanks;
 /// ```
 /// This has to be in here, due to how rust doctests work...
 pub fn puzzle_a(calorie_lists: &Vec<Vec<i32>>) -> i32 {
-    return get_highest_sum_and_index_of_list(&calorie_lists).0;
+    let mut sums = to_sums(calorie_lists);
+    sums.sort();
+    return sums.pop().unwrap();
 }
 
-fn get_highest_sum_and_index_of_list(calorie_lists: &Vec<Vec<i32>>) -> (i32, usize) {
-    let mut highest_total = -1;
-    let mut highest_index: usize = 0;
-
-    for (index, calorie_list) in calorie_lists.iter().enumerate() {
-        let current_total = calorie_list.iter().sum();
-        if current_total > highest_total {
-            highest_total = current_total;
-            highest_index = index;
-        }
-    }
-    return (highest_total, highest_index);
-}
-
-fn recursive_highest(calorie_lists: &Vec<Vec<i32>>, n: i32) -> i32 {
-    if n <= 0 {
-        return 0;
-    }
-    if n == 1 {
-        return get_highest_sum_and_index_of_list(&calorie_lists).0;
-    }
-    let (highest_sum, highest_index) = get_highest_sum_and_index_of_list(&calorie_lists);
-    let mut next_run = calorie_lists.clone();
-    next_run.remove(highest_index);
-    return highest_sum + recursive_highest(&next_run, n - 1);
+fn to_sums(calorie_list: &Vec<Vec<i32>>) -> Vec<i32> {
+    return calorie_list.iter().map(|l| l.iter().sum()).collect();
 }
 
 /// Get the top 3 highest sum of a group of vectors
@@ -47,7 +26,9 @@ fn recursive_highest(calorie_lists: &Vec<Vec<i32>>, n: i32) -> i32 {
 /// ```
 /// This has to be in here, due to how rust doctests work...
 pub fn puzzle_b(calorie_lists: &Vec<Vec<i32>>) -> i32 {
-    return recursive_highest(&calorie_lists, 3);
+    let mut sums = to_sums(calorie_lists);
+    sums.sort();
+    return sums.pop().unwrap() + sums.pop().unwrap() + sums.pop().unwrap();
 }
 
 #[cfg(test)]
@@ -55,7 +36,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_get_highest_sum_and_index_of_list() {
+    fn test_to_sums() {
         let vec1 = vec![
             vec![1000, 2000, 3000],
             vec![4000],
@@ -63,47 +44,7 @@ mod tests {
             vec![7000, 8000, 9000],
             vec![10000],
         ];
-        let expected = (24000, 3);
-        assert_eq!(get_highest_sum_and_index_of_list(&vec1), expected);
-    }
-
-    #[test]
-    fn test_recursive_highest() {
-        let vec1 = vec![
-            vec![1000, 2000, 3000],
-            vec![4000],
-            vec![5000, 6000],
-            vec![7000, 8000, 9000],
-            vec![10000],
-        ];
-        let expected = 35000;
-        assert_eq!(recursive_highest(&vec1, 2), expected);
-    }
-
-    #[test]
-    fn test_recursive_highest_0_case() {
-        let vec1 = vec![
-            vec![1000, 2000, 3000],
-            vec![4000],
-            vec![5000, 6000],
-            vec![7000, 8000, 9000],
-            vec![10000],
-        ];
-        let expected = 0;
-        assert_eq!(recursive_highest(&vec1, 0), expected);
-        assert_eq!(recursive_highest(&vec1, -1), expected);
-    }
-
-    #[test]
-    fn test_recursive_highest_1_case() {
-        let vec1 = vec![
-            vec![1000, 2000, 3000],
-            vec![4000],
-            vec![5000, 6000],
-            vec![7000, 8000, 9000],
-            vec![10000],
-        ];
-        let expected = 24000;
-        assert_eq!(recursive_highest(&vec1, 1), expected);
+        let expected = vec![6000, 4000, 11000, 24000, 10000];
+        assert_eq!(to_sums(&vec1), expected);
     }
 }
