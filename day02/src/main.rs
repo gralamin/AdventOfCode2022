e4397ffa28ffcc44758c198495d04536c9826e31
Glen Nelson <glen.nelson.1@gmail.com>
2022-12-01 23:35:30 -0700

    day02: Add math solution
    This makes everything numbers and just does raw number operations. I was slowed
    down by rust not actually having a modulus, and had to implement the old workaround.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,6 +1,8 @@
 use day02::load_no_blanks;
 use day02::puzzle_a;
+use day02::puzzle_a_sol2;
 use day02::puzzle_b;
+use day02::puzzle_b_sol2;
 
 fn main() {
     let filename = "input";
@@ -11,4 +13,12 @@ fn main() {
 
     let value_b = puzzle_b(&rounds);
     println!("Answer to 2nd question: {}", value_b);
+
+    println!("--------------------");
+    println!("Mathy solution");
+    let value_2 = puzzle_a_sol2(&rounds);
+    println!("Answer to 1st question: {}", value_2);
+
+    let value_b_2 = puzzle_b_sol2(&rounds);
+    println!("Answer to 2nd question: {}", value_b_2);
 }
