763ae92cd401bd5e5ba74ce328400023a2720293
Glen Nelson <glen.nelson.1@gmail.com>
2022-12-03 22:35:48 -0700

    day04: solution using if statements (O(n))

--- day04/src/lib.rs
+++ day04/src/lib.rs
@@ -2,22 +2,79 @@ extern crate filelib;
 
 pub use filelib::load_no_blanks;
 
+pub type DoublePair = (u32, u32, u32, u32);
+
+fn fully_contains(a_lower: u32, a_higher: u32, b_lower: u32, b_higher: u32) -> bool {
+    return a_lower <= b_lower && a_higher >= b_higher;
+}
+
+fn pairwise_fully_contains(a_lower: u32, a_higher: u32, b_lower: u32, b_higher: u32) -> bool {
+    return fully_contains(a_lower, a_higher, b_lower, b_higher)
+        || fully_contains(b_lower, b_higher, a_lower, a_higher);
+}
+
+/// Parse the line to just the lows and highs.
+/// ```
+/// let line = "2-4,6-8";
+/// assert_eq!(day04::parse_line(line), (2, 4, 6, 8));
+/// ```
+pub fn parse_line(s: &str) -> DoublePair {
+    let parsed: Vec<u32> = s
+        .split(",")
+        .map(|half| parse_pair(half))
+        .flatten()
+        .collect();
+    if parsed.len() != 4 {
+        return (999, 999, 999, 999);
+    }
+    return (parsed[0], parsed[1], parsed[2], parsed[3]);
+}
+
+fn parse_pair(s: &str) -> Vec<u32> {
+    return s.split("-").map(|v| v.parse::<u32>().unwrap()).collect();
+}
+
 /// Solution to puzzle_a entry point
 /// ```
-/// let vec1: Vec<String> = vec!["A Y", "B X", "C Z"].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day04::puzzle_a(&vec1), 1);
+/// let vec1: Vec<day04::DoublePair> = vec![(2,4,6,8), (2,3,4,5), (5,7,7,9), (2,8,3,7), (6,6,4,6), (2,6,4,8)];
+/// assert_eq!(day04::puzzle_a(&vec1), 2);
 /// ```
-pub fn puzzle_a(_input: &Vec<String>) -> i32 {
-    return 1;
+pub fn puzzle_a(input: &Vec<DoublePair>) -> usize {
+    return input
+        .iter()
+        .filter(|(a, b, c, d)| pairwise_fully_contains(*a, *b, *c, *d))
+        .count();
+}
+
+fn overlap_at_all(a_lower: u32, a_higher: u32, b_lower: u32, b_higher: u32) -> bool {
+    // Implemented only from a's perspective, call also with arguments reversed (pairwise)
+    // first check the lower cases, where a_lower is between b_lower and b_higher
+    if b_lower <= a_lower && b_higher >= a_lower {
+        return true;
+    }
+    // Check the higher cases, where a_higher is between b_lower and b_higher
+    if b_lower <= a_higher && b_higher >= a_higher {
+        return true;
+    }
+    // Finally check if one fully contains the other.
+    return fully_contains(a_lower, a_higher, b_lower, b_higher);
+}
+
+fn pairwise_overlap_at_all(a_lower: u32, a_higher: u32, b_lower: u32, b_higher: u32) -> bool {
+    return overlap_at_all(a_lower, a_higher, b_lower, b_higher)
+        || overlap_at_all(b_lower, b_higher, a_lower, a_higher);
 }
 
 /// Solution to puzzle_b entry point
 /// ```
-/// let vec1: Vec<String> = vec!["A Y", "B X", "C Z"].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day04::puzzle_b(&vec1), 2);
+/// let vec1: Vec<day04::DoublePair> = vec![(2,4,6,8), (2,3,4,5), (5,7,7,9), (2,8,3,7), (6,6,4,6), (2,6,4,8)];
+/// assert_eq!(day04::puzzle_b(&vec1), 4);
 /// ```
-pub fn puzzle_b(_input: &Vec<String>) -> i32 {
-    return 2;
+pub fn puzzle_b(input: &Vec<DoublePair>) -> usize {
+    return input
+        .iter()
+        .filter(|(a, b, c, d)| pairwise_overlap_at_all(*a, *b, *c, *d))
+        .count();
 }
 
 #[cfg(test)]
@@ -25,7 +82,24 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example() {
-        assert_eq!(1, 1);
+    fn test_pairwise_fully_contains() {
+        assert_eq!(pairwise_fully_contains(2, 4, 1, 5), true);
+        assert_eq!(pairwise_fully_contains(1, 5, 2, 4), true);
+        assert_eq!(pairwise_fully_contains(1, 5, 1, 4), true);
+        assert_eq!(pairwise_fully_contains(1, 5, 2, 5), true);
+        assert_eq!(pairwise_fully_contains(7, 8, 2, 5), false);
+    }
+
+    #[test]
+    fn test_pairwise_overlap_at_all() {
+        assert_eq!(pairwise_overlap_at_all(2, 4, 1, 5), true);
+        assert_eq!(pairwise_overlap_at_all(1, 5, 2, 4), true);
+        assert_eq!(pairwise_overlap_at_all(1, 5, 1, 4), true);
+        assert_eq!(pairwise_overlap_at_all(1, 5, 2, 5), true);
+        assert_eq!(pairwise_overlap_at_all(7, 8, 2, 5), false);
+        assert_eq!(pairwise_overlap_at_all(1, 2, 2, 5), true);
+        assert_eq!(pairwise_overlap_at_all(1, 3, 2, 5), true);
+        assert_eq!(pairwise_overlap_at_all(5, 6, 3, 5), true);
+        assert_eq!(pairwise_overlap_at_all(4, 6, 3, 5), true);
     }
 }
--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,14 +1,16 @@
 use day04::load_no_blanks;
+use day04::parse_line;
 use day04::puzzle_a;
 use day04::puzzle_b;
 
 fn main() {
     let filename = "input";
-    let template = load_no_blanks(filename);
+    let pairs_str = load_no_blanks(filename);
+    let pairs: Vec<(u32, u32, u32, u32)> = pairs_str.iter().map(|s| parse_line(s)).collect();
 
-    let value = puzzle_a(&template);
+    let value = puzzle_a(&pairs);
     println!("Answer to 1st question: {}", value);
 
-    let value_b = puzzle_b(&template);
+    let value_b = puzzle_b(&pairs);
     println!("Answer to 2nd question: {}", value_b);
 }
