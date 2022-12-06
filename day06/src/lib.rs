1975bd02d052f2b293b09da8189f0204e2cc1a19
Glen Nelson <glen.nelson.1@gmail.com>
2022-12-05 22:17:56 -0700

    Day06 solution
    This might be one of the simplest problems I've ever had.
    We only ever scan the full offset once, so O(N) there.
    
    Coverting the x characters to a set is also O(x). x is much smaller
    than N, so overall this is O(N)

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,31 +1,36 @@
 extern crate filelib;
 
-pub use filelib::load_no_blanks;
+pub use filelib::load;
+
+use std::collections::HashSet;
 
 /// Solution to puzzle_a entry point
 /// ```
-/// let vec1: Vec<String> = vec!["A Y", "B X", "C Z"].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day06::puzzle_a(&vec1), 1);
+/// let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+/// assert_eq!(day06::puzzle_a(input), 7);
 /// ```
-pub fn puzzle_a(_input: &Vec<String>) -> i32 {
-    return 1;
+pub fn puzzle_a(input: &str) -> usize {
+    return find_unique_char_pos(input, 4);
+}
+
+fn find_unique_char_pos(input: &str, num_unique: usize) -> usize {
+    // for 0 indexing
+    let num_unique_offset = num_unique - 1;
+    for i in num_unique_offset..input.len() {
+        let chars: Vec<char> = input[i-num_unique_offset..=i].chars().collect();
+        let set: HashSet<char> = chars.into_iter().collect();
+        if set.len() == num_unique {
+            return i + 1;
+        }
+    }
+    panic!("Never found");
 }
 
 /// Solution to puzzle_b entry point
 /// ```
-/// let vec1: Vec<String> = vec!["A Y", "B X", "C Z"].iter().map(|s| s.to_string()).collect();
-/// assert_eq!(day06::puzzle_b(&vec1), 2);
+/// let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+/// assert_eq!(day06::puzzle_b(input), 19);
 /// ```
-pub fn puzzle_b(_input: &Vec<String>) -> i32 {
-    return 2;
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_example() {
-        assert_eq!(1, 1);
-    }
+pub fn puzzle_b(input: &str) -> usize {
+    return find_unique_char_pos(input, 14);
 }
