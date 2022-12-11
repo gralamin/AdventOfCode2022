90d02e371aee66e8cd041aa17110d8d88eb5a7ca
Glen Nelson <glen.nelson.1@gmail.com>
2022-12-10 21:51:23 -0700

    Add day12 template

---
+++ day12/src/main.rs
@@ -0,0 +1,14 @@
+use day12::load_no_blanks;
+use day12::puzzle_a;
+use day12::puzzle_b;
+
+fn main() {
+    let filename = "input";
+    let template = load_no_blanks(filename);
+
+    let value = puzzle_a(&template);
+    println!("Answer to 1st question: {}", value);
+
+    let value_b = puzzle_b(&template);
+    println!("Answer to 2nd question: {}", value_b);
+}
