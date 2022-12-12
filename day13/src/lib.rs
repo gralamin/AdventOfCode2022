0d5a4860e5edfea3b27d89d8747e20978ee4511b
Glen Nelson <glen.nelson.1@gmail.com>
2022-12-11 23:14:44 -0700

    Add day 13 template

---
+++ day13/src/lib.rs
@@ -0,0 +1,31 @@
+extern crate filelib;
+
+pub use filelib::load_no_blanks;
+
+/// Solution to puzzle_a entry point
+/// ```
+/// let vec1: Vec<String> = vec!["A Y", "B X", "C Z"].iter().map(|s| s.to_string()).collect();
+/// assert_eq!(day13::puzzle_a(&vec1), 1);
+/// ```
+pub fn puzzle_a(_input: &Vec<String>) -> i32 {
+    return 1;
+}
+
+/// Solution to puzzle_b entry point
+/// ```
+/// let vec1: Vec<String> = vec!["A Y", "B X", "C Z"].iter().map(|s| s.to_string()).collect();
+/// assert_eq!(day13::puzzle_b(&vec1), 2);
+/// ```
+pub fn puzzle_b(_input: &Vec<String>) -> i32 {
+    return 2;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        assert_eq!(1, 1);
+    }
+}
