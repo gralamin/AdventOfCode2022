0911a99771e8b4e018aa891cda716d2009993c05
Glen Nelson <glen.nelson.1@gmail.com>
2022-12-19 03:02:36 -0700

    Still out of memory

--- day19/src/lib.rs
+++ day19/src/lib.rs
@@ -1,15 +1,15 @@
 extern crate filelib;
 
 pub use filelib::load_no_blanks;
+use rustc_hash::FxHashSet;
 use std::cmp::max;
 use std::collections::VecDeque;
 use std::thread;
 use std::thread::JoinHandle;
-use rustc_hash::FxHashSet;
 
 // id, ore_robot_cost, clay_robot_cost, obsidian_ore_cost, obsidian_clay_cost, geode_ore_cost, geode_obsidian_cost
 type BlueprintTuple = (usize, usize, usize, usize, usize, usize, usize);
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 struct Blueprint {
     id: usize,
     ore_robot_cost: usize,
@@ -34,14 +34,14 @@ impl Blueprint {
     }
 }
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 struct BuildMaterials {
     ore: usize,
     clay: usize,
     obsidian: usize,
 }
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 struct RobotInventory {
     ore: usize,
     clay: usize,
@@ -49,8 +49,50 @@ struct RobotInventory {
     geode: usize,
 }
 
+//std::mem::size_of is 64
+#[derive(Default, Hash, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
+struct SearchState {
+    ore: usize,
+    clay: usize,
+    obsidian: usize,
+    geode: usize,
+    ore_robot: usize,
+    clay_robot: usize,
+    obsidian_robot: usize,
+    geode_robot: usize,
+}
+
+
+impl SearchState {
+    fn new(r: RobotInventory, m: BuildMaterials, g: usize) -> SearchState {
+        return SearchState {
+            ore: m.ore,
+            clay: m.clay,
+            obsidian: m.obsidian,
+            geode: g,
+            ore_robot: r.ore,
+            clay_robot: r.clay,
+            obsidian_robot: r.obsidian,
+            geode_robot: r.geode,
+        };
+    }
+
+    fn collect(&self) -> SearchState {
+        return SearchState {
+            ore: self.ore + self.ore_robot,
+            clay: self.clay + self.clay_robot,
+            obsidian: self.obsidian + self.obsidian_robot,
+            geode: self.geode + self.geode_robot,
+            ore_robot: self.ore_robot,
+            clay_robot: self.clay_robot,
+            obsidian_robot: self.obsidian_robot,
+            geode_robot: self.geode_robot,
+        };
+    }
+}
+
 // I cache per blueprint, so don't need blueprint in key.
-type Cache = FxHashSet<(RobotInventory, BuildMaterials, usize)>;
+type Cache = FxHashSet<SearchState>;
 
 fn parse_blueprints(lines: &Vec<String>) -> Vec<Blueprint> {
     return lines.iter().map(|line| Blueprint::new(sscanf::sscanf!(line, "Blueprint {}: Each ore robot costs {} ore. Each clay robot costs {} ore. Each obsidian robot costs {} ore and {} clay. Each geode robot costs {} ore and {} obsidian.", usize, usize, usize, usize, usize, usize, usize).unwrap())).collect();
@@ -65,73 +107,13 @@ fn solve_single_blueprint(
     robot_state: RobotInventory,
     start_materials: BuildMaterials,
     num_turns: usize,
-    cache: &mut Cache,
 ) -> usize {
+    let mut cache = Cache::default();
     let mut geodes = robot_state.geode;
 
     if num_turns == 0 {
         return geodes;
     }
-
-    let mut queue: VecDeque<(RobotInventory, BuildMaterials, usize, usize)> = VecDeque::new();
-    queue.push_back((robot_state, start_materials, geodes, num_turns));
-
-    while let Some((cur_robot_state, cur_materials, cur_geodes, cur_turns)) = queue.pop_front() {
-        if cur_turns == 0 {
-            geodes = geodes.max(cur_geodes);
-            continue;
-        }
-        let cache_key = (cur_robot_state, cur_materials, cur_geodes);
-        if cache.contains(&cache_key) {
-            // We've already handled this
-            // Since we are BFS, its going to be a worse case of a later turn in the same state.
-            continue;
-        }
-        cache.insert(cache_key);
-
-        // You can think of a turn as made of three phases
-        // Construction start (pay costs)
-        // collection (Gain resources)
-        // construction finish (gain robots)
-        let build_queue =
-            get_possible_single_robots_to_build(blueprint, &cur_materials, &cur_robot_state);
-
-        for (possible_build, possible_material_state) in build_queue {
-            let (next_mats, add_geos) = collect_phase(cur_robot_state, possible_material_state);
-            let possible_robot = RobotInventory {
-                ore: possible_build.ore + cur_robot_state.ore,
-                clay: possible_build.clay + cur_robot_state.clay,
-                obsidian: possible_build.obsidian + cur_robot_state.obsidian,
-                geode: possible_build.geode + cur_robot_state.geode,
-            };
-            queue.push_back((
-                possible_robot,
-                next_mats,
-                cur_geodes + add_geos,
-                cur_turns - 1,
-            ));
-        }
-    }
-    return geodes;
-}
-
-fn collect_phase(
-    robot_state: RobotInventory,
-    start_materials: BuildMaterials,
-) -> (BuildMaterials, usize) {
-    let mut next_material_state = start_materials.clone();
-    next_material_state.ore += robot_state.ore;
-    next_material_state.clay += robot_state.clay;
-    next_material_state.obsidian += robot_state.obsidian;
-    return (next_material_state, robot_state.geode);
-}
-
-fn get_possible_single_robots_to_build(
-    blueprint: &Blueprint,
-    materials: &BuildMaterials,
-    robot_filter: &RobotInventory,
-) -> Vec<(RobotInventory, BuildMaterials)> {
-    let mut possiblities: Vec<(RobotInventory, BuildMaterials)> = vec![];
 
     // never a point in generating more of a resource then we need, with the exception of geodes.
     let max_ore_required: usize = max(
@@ -144,88 +126,84 @@ fn get_possible_single_robots_to_build(
     let max_clay_required = blueprint.obsidian_clay_cost;
     let max_obsidian_required = blueprint.geode_obsidian_cost;
 
-    if blueprint.ore_robot_cost <= materials.ore && robot_filter.ore < max_ore_required {
-        possiblities.push((
-            RobotInventory {
-                ore: 1,
-                clay: 0,
-                obsidian: 0,
-                geode: 0,
-            },
-            BuildMaterials {
-                ore: materials.ore - blueprint.ore_robot_cost,
-                clay: materials.clay,
-                obsidian: materials.obsidian,
-            },
-        ));
+    let start_state = SearchState::new(robot_state, start_materials, geodes);
+    let mut queue: VecDeque<(SearchState, usize)> = VecDeque::new();
+    queue.push_back((start_state, num_turns));
+
+    while let Some((mut state, cur_turns)) = queue.pop_front() {
+        if cur_turns == 0 {
+            geodes = geodes.max(state.geode);
+            continue;
+        }
+
+        // Heuristics here to cut out states
+        // Once we have the maximum number of robots, we can throw away the waste materials to reduce size of problem
+        if state.ore_robot >= max_ore_required {
+            state.ore = state.ore.min(max_ore_required);
+        }
+        if state.clay_robot >= max_clay_required {
+            state.clay = state.clay.min(max_clay_required);
+        }
+        if state.obsidian_robot >= max_obsidian_required {
+            state.obsidian = state.obsidian.min(max_obsidian_required);
+        }
+
+        if cache.contains(&state) {
+            // We've already handled this
+            // Since we are BFS, its going to be a worse case of a later turn in the same state.
+            continue;
+        }
+        cache.insert(state);
+
+        if cache.len() % 1000000 == 0 {            
+            println!("Cache is at size {}, ", cache.len());
+            println!("queue is at size {}, ", queue.len());
+            if cache.len() + queue.len() >= 100000000 {
+                panic!("Cache and queue combined are taking up almost all memory available");
+            }
+        }
+
+        // You can think of a turn as made of three phases
+        // Construction start (pay costs)
+        // collection (Gain resources)
+        // construction finish (gain robots)
+
+        let new_state_base = state.collect();
+
+        if state.ore_robot < max_ore_required && blueprint.ore_robot_cost <= state.ore {
+            let mut new_state = new_state_base.clone();
+            new_state.ore -= blueprint.ore_robot_cost;
+            new_state.ore_robot += 1;
+            queue.push_back((new_state, cur_turns - 1));
+        }
+        if state.clay_robot < max_clay_required && blueprint.clay_robot_cost <= state.ore {
+            let mut new_state = new_state_base.clone();
+            new_state.ore -= blueprint.clay_robot_cost;
+            new_state.clay_robot += 1;
+            queue.push_back((new_state, cur_turns - 1));
+        }
+        if blueprint.obsidian_clay_cost <= state.clay
+            && blueprint.obsidian_ore_cost <= state.ore
+            && state.obsidian_robot < max_obsidian_required
+        {
+            let mut new_state = new_state_base.clone();
+            new_state.ore -= blueprint.obsidian_ore_cost;
+            new_state.clay -= blueprint.obsidian_clay_cost;
+            new_state.obsidian_robot += 1;
+            queue.push_back((new_state, cur_turns - 1));
+        }
+        if blueprint.geode_obsidian_cost <= state.obsidian && blueprint.geode_ore_cost <= state.ore
+        {
+            let mut new_state = new_state_base.clone();
+            new_state.ore -= blueprint.geode_ore_cost;
+            new_state.obsidian -= blueprint.geode_obsidian_cost;
+            new_state.geode_robot += 1;
+            queue.push_back((new_state, cur_turns - 1));
+        } else {
+            queue.push_back((new_state_base, cur_turns - 1));
+        }
     }
-    if blueprint.clay_robot_cost <= materials.ore && robot_filter.clay < max_clay_required {
-        possiblities.push((
-            RobotInventory {
-                ore: 0,
-                clay: 1,
-                obsidian: 0,
-                geode: 0,
-            },
-            BuildMaterials {
-                ore: materials.ore - blueprint.clay_robot_cost,
-                clay: materials.clay,
-                obsidian: materials.obsidian,
-            },
-        ));
-    }
-    if blueprint.obsidian_clay_cost <= materials.clay
-        && blueprint.obsidian_ore_cost <= materials.ore
-        && robot_filter.obsidian < max_obsidian_required
-    {
-        possiblities.push((
-            RobotInventory {
-                ore: 0,
-                clay: 0,
-                obsidian: 1,
-                geode: 0,
-            },
-            BuildMaterials {
-                ore: materials.ore - blueprint.obsidian_ore_cost,
-                clay: materials.clay - blueprint.obsidian_clay_cost,
-                obsidian: materials.obsidian,
-            },
-        ));
-    }
-    if blueprint.geode_obsidian_cost <= materials.obsidian
-        && blueprint.geode_ore_cost <= materials.ore
-    {
-        possiblities.push((
-            RobotInventory {
-                ore: 0,
-                clay: 0,
-                obsidian: 0,
-                geode: 1,
-            },
-            BuildMaterials {
-                ore: materials.ore - blueprint.geode_ore_cost,
-                clay: materials.clay,
-                obsidian: materials.obsidian - blueprint.geode_obsidian_cost,
-            },
-        ));
-    } else {
-        // Optimization, handle the no build case here
-        // Never want to do it if we could build a geode, as building a geode will always be better... I think.
-        possiblities.push((
-            RobotInventory {
-                ore: 0,
-                clay: 0,
-                obsidian: 0,
-                geode: 0,
-            },
-            BuildMaterials {
-                ore: materials.ore,
-                clay: materials.clay,
-                obsidian: materials.obsidian,
-            },
-        ));
-    }
-    return possiblities;
+    return geodes;
 }
 
 /// Solution to puzzle_a entry point
@@ -258,14 +236,12 @@ fn solve_puzzle_a_state(blueprints: Vec<Blueprint>, num_rounds: usize) -> usize 
     let handles: Vec<JoinHandle<usize>> = blueprints
         .into_iter()
         .map(|b| {
+            let r = robot_types.clone();
+            let s = start_mats.clone();
             return thread::Builder::new()
                 .name(format!("blueprint-thread-{}", b.id).to_string())
                 .spawn(move || {
-                    let mut cache = Cache::default();
-                    return calculate_quality(
-                        &b,
-                        solve_single_blueprint(&b, robot_types, start_mats, num_rounds, &mut cache),
-                    );
+                    return calculate_quality(&b, solve_single_blueprint(&b, r, s, num_rounds));
                 })
                 .unwrap();
         })
@@ -305,17 +281,12 @@ fn solve_puzzle_b_state(blueprints: Vec<Blueprint>, num_rounds: usize) -> usize 
     let handles: Vec<JoinHandle<usize>> = blueprints
         .into_iter()
         .map(|b| {
+            let r = robot_types.clone();
+            let s = start_mats.clone();
             return thread::Builder::new()
                 .name(format!("blueprint-thread-{}", b.id).to_string())
                 .spawn(move || {
-                    let mut cache = Cache::default();
-                    return solve_single_blueprint(
-                        &b,
-                        robot_types,
-                        start_mats,
-                        num_rounds,
-                        &mut cache,
-                    );
+                    return solve_single_blueprint(&b, r, s, num_rounds);
                 })
                 .unwrap();
         })
